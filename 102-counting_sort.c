d3761e05876fac1481eea3fa31c3a193e242e5a5
zakaria ahmed <116184228+zachafs@users.noreply.github.com>
2023-07-21 14:44:06 -0400

    Update 102-counting_sort.c
    102

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -3,50 +3,67 @@
 #include <stdio.h>
 
 /**
-  * counting_sort - Afunction that sorts an array using counting algorithm.
-  * @array: The array to sort.
-  * @size: The length of the array.
-  * Return: Nothing.
-  */
+ * counting_sort - A function that sorts an array using counting algorithm.
+ * @array: The array to sort.
+ * @size: The length of the array.
+ * Return: Nothing.
+ */
 void counting_sort(int *array, size_t size)
 {
-	unsigned int i = 1;
-	int *counter = NULL, k = 0, j = 0;
+    unsigned int i = 0;
+    int *counter = NULL, *output = NULL, k = 0, j = 0;
 
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	k = array[0];
-	for (; i < size; i++)
-	{
-		if (array[i] > k)
-			k = array[i];
-	}
+    // Find the maximum element in the array
+    k = array[0];
+    for (i = 1; i < size; i++)
+    {
+        if (array[i] > k)
+            k = array[i];
+    }
 
-	counter = malloc(sizeof(int) * (k + 1));
-	if (counter == NULL)
-		return;
+    // Allocate memory for the counter array
+    counter = malloc(sizeof(int) * (k + 1));
+    if (counter == NULL)
+        return;
 
-	for (j = 0; j <= k; j++)
-		counter[j] = 0;
-	for (i = 0; i < size; i++)
-		counter[array[i]] += 1;
-	for (j = 0; j < k; j++)
-	{
-		counter[j + 1] += counter[j];
-		printf("%d, ", counter[j]);
-	}
-	counter[j + 1] += counter[j];
-	printf("%d\n", counter[j + 1]);
-	for (i = 0; i < size; i++)
-	{
-		j = counter[array[i]] - 1;
-		if (array[i] != array[j])
-		{
-			k = array[i];
-			array[i] = array[j];
-			array[j] = k;
-		}
-	}
-	free(counter);
+    // Initialize counter array to 0
+    for (j = 0; j <= k; j++)
+        counter[j] = 0;
+
+    // Count the occurrences of each element
+    for (i = 0; i < size; i++)
+        counter[array[i]] += 1;
+
+    // Update counter array to contain actual positions in the output array
+    for (j = 1; j <= k; j++)
+        counter[j] += counter[j - 1];
+
+    // Create the output array
+    output = malloc(sizeof(int) * size);
+    if (output == NULL)
+    {
+        free(counter);
+        return;
+    }
+
+    // Place elements in their correct positions in the output array
+    for (i = size - 1; i < size; i--) // i is unsigned, so we check for i < size to avoid underflow.
+    {
+        j = array[i];
+        output[counter[j] - 1] = array[i];
+        counter[j] -= 1;
+    }
+
+    // Copy the sorted elements back to the original array
+    for (i = 0; i < size; i++)
+    {
+        array[i] = output[i];
+    }
+
+    // Free allocated memory
+    free(counter);
+    free(output);
 }

284ca3a8ef5477233126779c472a522946a7eee4
zakaria ahmed <116184228+zachafs@users.noreply.github.com>
2023-07-21 14:15:53 -0400

    Update 100-shell_sort.c
    while to for

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,29 +9,27 @@
 void shell_sort(int *array, size_t size)
 {
 	unsigned int i = 0, j = 0, gap = 0;
-	int aux = 0;
+	int aux;
+{
+	unsigned int i, j, gap;
+	int aux;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-
-	for (; gap > 0; gap = (gap - 1) / 3)
+	for (gap = size / 2; gap > 0; gap /= 2)
 	{
 		for (i = gap; i < size; i++)
 		{
 			aux = array[i];
-			for (j = i; j >= gap && array[j - gap] > aux;
-					j -= gap)
-			{
-				if (array[j] != array[j - gap])
-					array[j] = array[j - gap];
-			}
-			if (array[j] != aux)
-				array[j] = aux;
 
+			for (j = i; j >= gap && array[j - gap] > aux; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+
+			array[j] = aux;
 		}
+
 		print_array(array, size);
 	}
-}
